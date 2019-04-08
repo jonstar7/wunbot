db77e1640bc9c752c83610e6228ef0938e8162b8
coruscar <jonathan22711@gmail.com>
2019-04-07 20:50:00 -0700

    more ps testing

--- tests/processes.py
+++ tests/processes.py
@@ -2,7 +2,11 @@ import psutil
 for proc in psutil.process_iter():
     try:
         pinfo = proc.as_dict(attrs=['name'])
+        
     except psutil.NoSuchProcess:
         pass
     else:
-        print(pinfo)
+        # print(pinfo)
+        # print("coolio")
+        if "chrome.exe" in pinfo: 
+            print("found chrome")
